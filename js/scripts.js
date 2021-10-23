1964cca1b1579c37742c0655eee9fbc5fb979b5c
AbhishekMore-1 <abhishekbabajim@gmail.com>
2021-10-23 15:53:11 +0530

    Animation added and other modifications

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,16 @@ function displaySpeaker(SpeakerId){
     element.classList.replace("speaker-card","speaker-card-active");
     PreviousSpeakerCardIndex = SpeakerId;
 }
+
+function toggleNavBar(){
+    let NavList = document.getElementById("tedx");
+    let NavButton = document.getElementsByClassName("navbar-toggler")[0];
+    if(NavList.classList.contains("show")){
+        NavList.classList.remove("show");
+        NavButton.classList.add("collapsed");
+    }
+    else{
+        NavList.classList.add("show");
+        NavButton.classList.remove("collapsed");
+    }
+}

7b19747c458257f2ed692d5eda6a90c1f5e1b794
niranjangirhe <niranjangirheindia@gmail.com>
2021-11-23 11:03:28 +0530

    safari circle

--- js/squarefixer.js
+++ js/squarefixer.js
@@ -1,7 +1,18 @@
 var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 if(isSafari)
 {
-    console.log("safari");
+    var link = document.createElement('link'); 
+  
+    // set the attributes for link element
+       link.rel = 'stylesheet'; 
+  
+    link.type = 'text/css';
+  
+    link.href = 'css/circleimg.css'; 
+
+    // Get HTML head element to append 
+    // link element to it 
+    document.getElementsByTagName('HEAD')[0].appendChild(link); 
 }
 else{
     console.log("something else");
